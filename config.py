42d3e956fc5f3bf105604b64348a5f5636a14605
Patel1545 <patelprinci920@gmail.com>
2025-05-24 23:35:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "29267685"
+api_hash = "9aea863501d41261e6c75ad565b6e1e1"
+bot_token = "7909537410:AAE5jlEuCSdN8do2gdWAM1raZXusZjIdLDE"
+auth_users = [7916516048]
